37475a69aaeca6d43a2785f33a0e06aef9b1993b
Allan Krueger <klimber.mail@gmail.com>
2018-10-04 22:50:34 -0300

    Terminando o exercício de JUnit

--- Academia DEV/gerenciador-times-futebol-klimber/src/main/java/br/com/codenation/DesafioMeuTimeApplication.java
+++ Academia DEV/gerenciador-times-futebol-klimber/src/main/java/br/com/codenation/DesafioMeuTimeApplication.java
@@ -8,43 +8,36 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.TreeMap;
 
-
 import br.com.codenation.desafio.annotation.Desafio;
 import br.com.codenation.desafio.app.MeuTimeInterface;
 import br.com.codenation.desafio.exceptions.*;
 
 public class DesafioMeuTimeApplication implements MeuTimeInterface {
-	
+
 	HashMap<Long, Time> listaTime = new HashMap<Long, Time>();
 	HashMap<Long, Jogador> listaJogador = new HashMap<Long, Jogador>();
 
 	@Desafio("incluirTime")
-	public void incluirTime(Long id, String nome, LocalDate dataCriacao, String corUniformePrincipal, String corUniformeSecundario) {
-		if(listaTime.containsKey(id)) {
+	public void incluirTime(Long id, String nome, LocalDate dataCriacao, String corUniformePrincipal,
+			String corUniformeSecundario) {
+		if (listaTime.containsKey(id)) {
 			throw new IdentificadorUtilizadoException();
-			
+
 		} else {
 			Time timeadd = new Time();
 			timeadd.nome = nome;
 			timeadd.dataCriacao = dataCriacao;
 			timeadd.corUniformePrincipal = corUniformePrincipal;
 			timeadd.corUniformeSecundario = corUniformeSecundario;
-			listaTime.put(id,timeadd);	
+			listaTime.put(id, timeadd);
 		}
 	}
 
 	@Desafio("incluirJogador")
-	public void incluirJogador(Long id, Long idTime, String nome, LocalDate dataNascimento, Integer nivelHabilidade, BigDecimal salario) {
-		
-		if(listaJogador.containsKey(id)) {
-			throw new IdentificadorUtilizadoException();
-		}
-		
-		if(listaTime.containsKey(idTime)==false) {
-			throw new TimeNaoEncontradoException();
-		}
-		
-		if(listaJogador.containsKey(id)==false && listaTime.containsKey(idTime)) {
+	public void incluirJogador(Long id, Long idTime, String nome, LocalDate dataNascimento, Integer nivelHabilidade,
+			BigDecimal salario) {
+
+		if (listaJogador.containsKey(id) == false && listaTime.containsKey(idTime)) {
 			Jogador jogadoradd = new Jogador();
 			jogadoradd.idTime = idTime;
 			jogadoradd.nome = nome;
@@ -52,20 +45,29 @@ public class DesafioMeuTimeApplication implements MeuTimeInterface {
 			jogadoradd.nivelHabilidade = nivelHabilidade;
 			jogadoradd.salario = salario;
 			listaJogador.put(id, jogadoradd);
-			
+
+		} else {
+
+			if (listaTime.containsKey(idTime) == false) {
+				throw new TimeNaoEncontradoException();
+			}
+
+			if (listaJogador.containsKey(id)) {
+				throw new IdentificadorUtilizadoException();
+			}
 		}
 	}
 
 	@Desafio("definirCapitao")
 	public void definirCapitao(Long idJogador) {
-		
-		if(listaJogador.containsKey(idJogador)) {
+
+		if (listaJogador.containsKey(idJogador)) {
 			Jogador jogadorcapitao = listaJogador.get(idJogador);
-			
+
 			for (Long id : listaJogador.keySet()) {
 				Jogador outrojogador = listaJogador.get(id);
-			
-				if(outrojogador.idTime == jogadorcapitao.idTime) {
+
+				if (outrojogador.idTime == jogadorcapitao.idTime) {
 					outrojogador.capitao = false;
 				}
 			}
@@ -77,29 +79,29 @@ public class DesafioMeuTimeApplication implements MeuTimeInterface {
 
 	@Desafio("buscarCapitaoDoTime")
 	public Long buscarCapitaoDoTime(Long idTime) {
-		
+
 		Long idCapitao = 0L;
 		int contador = 0;
-		
-		if(listaTime.containsKey(idTime)==false) {
+
+		if (listaTime.containsKey(idTime) == false) {
 			throw new TimeNaoEncontradoException();
-			
+
 		} else {
-			
-			for(Long id : listaJogador.keySet()) {
+
+			for (Long id : listaJogador.keySet()) {
 				Jogador jogador = listaJogador.get(id);
-				if(jogador.idTime==idTime) {
-					if(jogador.capitao==true) {
+				if (jogador.idTime == idTime) {
+					if (jogador.capitao == true) {
 						idCapitao = id;
 						contador += 1;
 					}
 				}
 			}
 		}
-		
-		if(contador == 0) {
+
+		if (contador == 0) {
 			throw new CapitaoNaoInformadoException();
-			
+
 		} else {
 			return idCapitao;
 		}
@@ -107,8 +109,8 @@ public class DesafioMeuTimeApplication implements MeuTimeInterface {
 
 	@Desafio("buscarNomeJogador")
 	public String buscarNomeJogador(Long idJogador) {
-		
-		if(listaJogador.containsKey(idJogador)) {
+
+		if (listaJogador.containsKey(idJogador)) {
 			Jogador jogador = listaJogador.get(idJogador);
 			return jogador.nome;
 		} else {
@@ -118,8 +120,8 @@ public class DesafioMeuTimeApplication implements MeuTimeInterface {
 
 	@Desafio("buscarNomeTime")
 	public String buscarNomeTime(Long idTime) {
-		
-		if(listaTime.containsKey(idTime)) {
+
+		if (listaTime.containsKey(idTime)) {
 			Time time = listaTime.get(idTime);
 			return time.nome;
 		} else {
@@ -129,18 +131,18 @@ public class DesafioMeuTimeApplication implements MeuTimeInterface {
 
 	@Desafio("buscarJogadoresDoTime")
 	public List<Long> buscarJogadoresDoTime(Long idTime) {
-		
+
 		ArrayList<Long> listaJogadores = new ArrayList<Long>();
-		if(listaTime.containsKey(idTime)) {
-			for(Long id : listaJogador.keySet()) {
+		if (listaTime.containsKey(idTime)) {
+			for (Long id : listaJogador.keySet()) {
 				Jogador jogador = listaJogador.get(id);
-				if(jogador.idTime == idTime) {
+				if (jogador.idTime == idTime) {
 					listaJogadores.add(id);
 				}
 			}
 			Collections.sort(listaJogadores);
 			return listaJogadores;
-			
+
 		} else {
 			throw new TimeNaoEncontradoException();
 		}
@@ -148,44 +150,44 @@ public class DesafioMeuTimeApplication implements MeuTimeInterface {
 
 	@Desafio("buscarMelhorJogadorDoTime")
 	public Long buscarMelhorJogadorDoTime(Long idTime) {
-		
+
 		Long idmelhorjogador = Long.MAX_VALUE;
 		Integer habilidademax = 0;
-		if(listaTime.containsKey(idTime)) {
-			for(Long id : listaJogador.keySet()) {
+		if (listaTime.containsKey(idTime)) {
+			for (Long id : listaJogador.keySet()) {
 				Jogador jogador = listaJogador.get(id);
-				if(jogador.idTime == idTime) {
-					if(jogador.nivelHabilidade > habilidademax) {
+				if (jogador.idTime == idTime) {
+					if (jogador.nivelHabilidade > habilidademax) {
 						habilidademax = jogador.nivelHabilidade;
 						idmelhorjogador = id;
 					} else if (jogador.nivelHabilidade == habilidademax) {
-						if(id < idmelhorjogador) {
+						if (id < idmelhorjogador) {
 							idmelhorjogador = id;
 						}
 					}
-					
+
 				}
 			}
 			return idmelhorjogador;
 		} else {
 			throw new TimeNaoEncontradoException();
-		}		
+		}
 	}
 
 	@Desafio("buscarJogadorMaisVelho")
 	public Long buscarJogadorMaisVelho(Long idTime) {
-		
+
 		Long idmaisvelho = Long.MAX_VALUE;
 		LocalDate data = LocalDate.now();
-		if(listaTime.containsKey(idTime)) {
-			for(Long id : listaJogador.keySet()) {
+		if (listaTime.containsKey(idTime)) {
+			for (Long id : listaJogador.keySet()) {
 				Jogador jogador = listaJogador.get(id);
-				if(jogador.idTime == idTime) {
-					if(data.isAfter(jogador.dataNascimento)){
+				if (jogador.idTime == idTime) {
+					if (data.isAfter(jogador.dataNascimento)) {
 						data = jogador.dataNascimento;
 						idmaisvelho = id;
-					}else if (data.isEqual(jogador.dataNascimento)) {
-						if(idmaisvelho > id) {
+					} else if (data.isEqual(jogador.dataNascimento)) {
+						if (idmaisvelho > id) {
 							idmaisvelho = id;
 						}
 					}
@@ -199,12 +201,10 @@ public class DesafioMeuTimeApplication implements MeuTimeInterface {
 
 	@Desafio("buscarTimes")
 	public List<Long> buscarTimes() {
-		
-		
-		
-		if(listaTime.isEmpty()) {
+
+		if (listaTime.isEmpty()) {
 			return new ArrayList<Long>();
-			
+
 		} else {
 			ArrayList<Long> listaTimes = new ArrayList<Long>(listaTime.keySet());
 			Collections.sort(listaTimes);
@@ -214,24 +214,27 @@ public class DesafioMeuTimeApplication implements MeuTimeInterface {
 
 	@Desafio("buscarJogadorMaiorSalario")
 	public Long buscarJogadorMaiorSalario(Long idTime) {
-		
+
 		Long idmaiorsalario = Long.MAX_VALUE;
 		BigDecimal maiorsalario = BigDecimal.ZERO;
-		if(listaTime.containsKey(idTime)) {
-			for(Long id : listaJogador.keySet()) {
+		if (listaTime.containsKey(idTime)) {
+			for (Long id : listaJogador.keySet()) {
 				Jogador jogador = listaJogador.get(id);
-				if(jogador.salario.compareTo(maiorsalario) == 1) {
-					maiorsalario = jogador.salario;
-					idmaiorsalario = id;
-					
-				} else if(maiorsalario.equals(jogador.salario)) {
-					if(id < idmaiorsalario) {
+				if (jogador.idTime == idTime) {
+					if (jogador.salario.compareTo(maiorsalario) == 1) {
+						maiorsalario = jogador.salario;
 						idmaiorsalario = id;
+
+					} else if (maiorsalario.equals(jogador.salario)) {
+						if (id < idmaiorsalario) {
+							idmaiorsalario = id;
+						}
 					}
+
 				}
 			}
 			return idmaiorsalario;
-			
+
 		} else {
 			throw new TimeNaoEncontradoException();
 		}
@@ -239,11 +242,9 @@ public class DesafioMeuTimeApplication implements MeuTimeInterface {
 
 	@Desafio("buscarSalarioDoJogador")
 	public BigDecimal buscarSalarioDoJogador(Long idJogador) {
-		
-		if(listaJogador.containsKey(idJogador)) {
-			Jogador jogador = listaJogador.get(idJogador);
-			return jogador.salario;
-			
+
+		if (listaJogador.containsKey(idJogador)) {
+			return listaJogador.get(idJogador).salario;
 		} else {
 			throw new JogadorNaoEncontradoException();
 		}
@@ -251,25 +252,46 @@ public class DesafioMeuTimeApplication implements MeuTimeInterface {
 
 	@Desafio("buscarTopJogadores")
 	public List<Long> buscarTopJogadores(Integer top) {
-		TreeMap<Integer, Long> listatop = new TreeMap<Integer, Long>();
-		if(listaJogador.isEmpty()) {
+		
+		if (listaJogador.isEmpty()) {
 			return new ArrayList<Long>();
-		}else {
+		} else {
+			ArrayList<Long> idsJogador = new ArrayList<Long>();
+			ArrayList<Integer> habilidades = new ArrayList<Integer>();
 			for(Long id : listaJogador.keySet()) {
-				listatop.put(listaJogador.get(id).nivelHabilidade, id);			
+				idsJogador.add(id);
+				habilidades.add(listaJogador.get(id).nivelHabilidade);
 			}
-			List<Long> arraytop = new ArrayList<Long>(listatop.values()).subList(0, top);
-			Collections.reverse(arraytop);
-			return arraytop;
+			Integer counter = 1;
+			while(counter != 0) {
+				counter = 0;
+				for(int i = 1; i < idsJogador.size();i++) {
+					if(habilidades.get(i) > habilidades.get(i-1)) {
+						idsJogador.add(0, idsJogador.get(i));
+						idsJogador.remove(i+1);
+						habilidades.add(0, habilidades.get(i));
+						habilidades.remove(i+1);
+						counter += 1;
+					} else if(habilidades.get(i) == habilidades.get(i-1)) {
+						if(idsJogador.get(i) < idsJogador.get(i-1)) {
+							idsJogador.add(0, idsJogador.get(i));
+							idsJogador.remove(i+1);
+							habilidades.add(0, habilidades.get(i));
+							habilidades.remove(i+1);
+						}
+					}
+				}
+			}
+			return idsJogador.subList(0, top);
 		}
 	}
 
 	@Desafio("buscarCorCamisaTimeDeFora")
 	public String buscarCorCamisaTimeDeFora(Long timeDaCasa, Long timeDeFora) {
-		if(listaTime.containsKey(timeDaCasa) && listaTime.containsKey(timeDeFora)) {
+		if (listaTime.containsKey(timeDaCasa) && listaTime.containsKey(timeDeFora)) {
 			Time dacasa = listaTime.get(timeDaCasa);
 			Time defora = listaTime.get(timeDeFora);
-			if(dacasa.corUniformePrincipal == defora.corUniformePrincipal) {
+			if (dacasa.corUniformePrincipal == defora.corUniformePrincipal) {
 				return defora.corUniformeSecundario;
 			} else {
 				return defora.corUniformePrincipal;
--- Academia DEV/gerenciador-times-futebol-klimber/src/test/java/br/com/codenation/DesafioMeuTimeApplicationTest.java
+++ Academia DEV/gerenciador-times-futebol-klimber/src/test/java/br/com/codenation/DesafioMeuTimeApplicationTest.java
@@ -1,8 +1,11 @@
 package br.com.codenation;
 
+import static org.junit.Assert.*;
+
 import java.math.BigDecimal;
 import java.math.BigInteger;
 import java.time.LocalDate;
+import java.util.ArrayList;
 
 import org.junit.Assert;
 import org.junit.Test;
@@ -11,46 +14,292 @@ import br.com.codenation.desafio.exceptions.*;
 
 public class DesafioMeuTimeApplicationTest {
 
+	public void incluirTimes(DesafioMeuTimeApplication teste) {
+		teste.incluirTime(1l, "Real Madrid", LocalDate.of(2016, 1, 1), "Branco", "Azul");
+		teste.incluirTime(2L, "Barcelona", LocalDate.of(2018, 2, 20), "Prata", "Preto");
+		teste.incluirTime(3L, "Fluminense", LocalDate.of(2000, 03, 14), "Branco", "Vermelho");
+	}
+
+	public void incluirJogadores(DesafioMeuTimeApplication teste) {
+		teste.incluirJogador(1L, 1L, "Allan1", LocalDate.of(1993, 03, 24), 30, BigDecimal.valueOf(2000));
+		teste.incluirJogador(2L, 1L, "Allan2", LocalDate.of(1993, 03, 24), 20, BigDecimal.valueOf(3000));
+		teste.incluirJogador(3L, 1L, "Allan3", LocalDate.of(1993, 03, 24), 10, BigDecimal.valueOf(1000));
+		teste.incluirJogador(5L, 2L, "Leonardo1", LocalDate.of(1996, 10, 1), 90, BigDecimal.valueOf(0));
+		teste.incluirJogador(4L, 2L, "Leonardo2", LocalDate.of(1995, 10, 1), 90, BigDecimal.valueOf(100));
+		teste.incluirJogador(6L, 2L, "Leonardo3", LocalDate.of(1994, 10, 1), 45, BigDecimal.valueOf(200));
+		teste.incluirJogador(7L, 3L, "Perion1", LocalDate.of(1995, 12, 8), 4, BigDecimal.valueOf(400));
+		teste.incluirJogador(9L, 3L, "Perion2", LocalDate.of(1995, 12, 8), 2, BigDecimal.valueOf(500));
+		teste.incluirJogador(8L, 3L, "Perion3", LocalDate.of(1995, 12, 8), 5, BigDecimal.valueOf(300));
+	}
+
 	@Test
 	public void testeIncluirTime() {
 		DesafioMeuTimeApplication teste = new DesafioMeuTimeApplication();
 
-		teste.incluirTime(1l, "Real Madrid", LocalDate.of(2016, 1, 1), "Branco", "Azul");
+		incluirTimes(teste);
+
+		Time timeTeste = teste.listaTime.get(1L);
+
+		Assert.assertEquals("Nome Incorreto", "Real Madrid", timeTeste.nome);
+		Assert.assertEquals("Data incorreta", LocalDate.of(2016, 1, 1), timeTeste.dataCriacao);
+		Assert.assertEquals("Cor Principal incorreta", "Branco", timeTeste.corUniformePrincipal);
+		Assert.assertEquals("Cor Secundária incorreta", "Azul", timeTeste.corUniformeSecundario);
 
 		try {
 			teste.incluirTime(1l, "Real Madrid", LocalDate.of(2016, 1, 1), "Branco", "Azul");
 			Assert.assertTrue("Deveria dar exceção", false);
 		} catch (Exception e) {
-			if (e.getCause() instanceof IdentificadorUtilizadoException) {
-				Assert.assertTrue(false);
-			}
+			Assert.assertTrue("Exceção Incorreta", e instanceof IdentificadorUtilizadoException);
 		}
 	}
 
 	@Test
 	public void testeIncluirJogador() {
 		DesafioMeuTimeApplication teste = new DesafioMeuTimeApplication();
-		
+
 		try {
 			teste.incluirJogador(1L, 1L, "Allan Krueger", LocalDate.of(1993, 03, 24), 3, BigDecimal.valueOf(2000));
 			Assert.assertTrue("Deveria dar exceção de time não encontrado", false);
-		} catch (TimeNaoEncontradoException e) {
-			if (e.getCause() instanceof TimeNaoEncontradoException) {
-				Assert.assertTrue(false);
-			}
+		} catch (Exception e) {
+			Assert.assertTrue("Deveria ser TimeNãoEncontradoException, mas é " + e,
+					e instanceof TimeNaoEncontradoException);
 		}
-		teste.incluirTime(1l, "Real Madrid", LocalDate.of(2016, 1, 1), "Branco", "Azul");
-		teste.incluirJogador(1L, 1L, "Allan Krueger", LocalDate.of(1993, 03, 24), 3, BigDecimal.valueOf(2000));
-		
+
+		incluirTimes(teste);
+		incluirJogadores(teste);
+
+		Jogador jogadorTeste = teste.listaJogador.get(3L);
+
+		Assert.assertEquals("Id do Time Incorreto", Long.valueOf(1), jogadorTeste.idTime);
+		Assert.assertEquals("Nome Incorreto", "Allan3", jogadorTeste.nome);
+		Assert.assertEquals("Data Nasc. Incorreta", LocalDate.of(1993, 03, 24), jogadorTeste.dataNascimento);
+		Assert.assertEquals("Habilidade Incorreta", Integer.valueOf(10), jogadorTeste.nivelHabilidade);
+		Assert.assertEquals("Salário Incorreto", BigDecimal.valueOf(1000), jogadorTeste.salario);
+
 		try {
 			teste.incluirJogador(1L, 1L, "Allan Krueger", LocalDate.of(1993, 03, 24), 3, BigDecimal.valueOf(2000));
-			Assert.assertTrue("Deveria dar exceção de identificador existente", false);
+			Assert.assertTrue("Deveria dar exceção", false);
 		} catch (Exception e) {
-			if (e.getCause() instanceof IdentificadorUtilizadoException) {
-				Assert.assertTrue(false);
-			}
+			Assert.assertTrue("Exceção deveria ser IdentificadorUtilizadoException, mas é" + e,
+					e instanceof IdentificadorUtilizadoException);
 		}
 
 	}
 
+	@Test
+	public void testeDefinirCapitao() {
+		DesafioMeuTimeApplication teste = new DesafioMeuTimeApplication();
+
+		incluirTimes(teste);
+		incluirJogadores(teste);
+
+		try {
+			teste.definirCapitao(Long.valueOf(10));
+			Assert.assertTrue("Deveria dar Exceção", false);
+		} catch (Exception e) {
+			Assert.assertTrue("Deveria ser JogadorNaoEncontradoException", e instanceof JogadorNaoEncontradoException);
+		}
+
+		teste.definirCapitao(Long.valueOf(5));
+		Assert.assertEquals("Deveria ser Capitão", true, teste.listaJogador.get(5L).capitao);
+
+	}
+
+	@Test
+	public void testeBuscaCapitaoTime() {
+		DesafioMeuTimeApplication teste = new DesafioMeuTimeApplication();
+
+		try {
+			teste.buscarCapitaoDoTime(1L);
+			Assert.assertTrue("Deveria dar Exceção", false);
+		} catch (Exception e) {
+			Assert.assertTrue("Deveria ser TimeNaoEncontradoException", e instanceof TimeNaoEncontradoException);
+		}
+
+		incluirTimes(teste);
+		incluirJogadores(teste);
+
+		try {
+			teste.buscarCapitaoDoTime(1L);
+			Assert.assertTrue("Deveria dar Exceção", false);
+		} catch (Exception e) {
+			Assert.assertTrue("Deveria ser CapitaoNaoInformadoException", e instanceof CapitaoNaoInformadoException);
+		}
+
+		teste.definirCapitao(5L);
+		Assert.assertEquals("Deveria ser capitao", Long.valueOf(5), teste.buscarCapitaoDoTime(2L));
+	}
+
+	@Test
+	public void testeBuscarNomeJogador() {
+		DesafioMeuTimeApplication teste = new DesafioMeuTimeApplication();
+
+		try {
+			teste.buscarNomeJogador(3L);
+			Assert.assertTrue("Deveria dar exceção", false);
+		} catch (Exception e) {
+			Assert.assertTrue("Deveria ser JogadorNaoEncontradoException", e instanceof JogadorNaoEncontradoException);
+		}
+
+		incluirTimes(teste);
+		incluirJogadores(teste);
+
+		Assert.assertEquals("Deveria ser Allan1", "Allan1", teste.buscarNomeJogador(1L));
+	}
+
+	@Test
+	public void testeBuscarNomeTime() {
+		DesafioMeuTimeApplication teste = new DesafioMeuTimeApplication();
+
+		try {
+			teste.buscarNomeTime(3L);
+			Assert.assertTrue("Deveria dar exceção", false);
+		} catch (Exception e) {
+			Assert.assertTrue("Deveria ser TimeNaoEncontradoException", e instanceof TimeNaoEncontradoException);
+		}
+
+		incluirTimes(teste);
+
+		Assert.assertEquals("Deveria ser Real Madrid", "Real Madrid", teste.buscarNomeTime(1L));
+	}
+
+	@Test
+	public void testeBuscarJogadoresDoTime() {
+		DesafioMeuTimeApplication teste = new DesafioMeuTimeApplication();
+
+		try {
+			teste.buscarJogadoresDoTime(3L);
+			Assert.assertTrue("Deveria dar Exceção", false);
+		} catch (Exception e) {
+			Assert.assertTrue("Deveria ser TimeNaoEncontradoException", e instanceof TimeNaoEncontradoException);
+		}
+
+		incluirTimes(teste);
+		incluirJogadores(teste);
+
+		ArrayList<Long> lista = new ArrayList<Long>();
+		lista.add(7L);
+		lista.add(8L);
+		lista.add(9L);
+
+		Assert.assertEquals(lista, teste.buscarJogadoresDoTime(3L));
+	}
+
+	@Test
+	public void testeBuscarMelhorJogadorDoTime() {
+		DesafioMeuTimeApplication teste = new DesafioMeuTimeApplication();
+
+		try {
+			teste.buscarMelhorJogadorDoTime(1L);
+			Assert.assertTrue("Deveria dar Exceção", false);
+		} catch (Exception e) {
+			Assert.assertTrue("Deveria ser TimeNaoEncontradoException", e instanceof TimeNaoEncontradoException);
+		}
+
+		incluirTimes(teste);
+		incluirJogadores(teste);
+
+		Assert.assertEquals("Deveria ser o 4", Long.valueOf(4), teste.buscarMelhorJogadorDoTime(2L));
+	}
+	
+	@Test
+	public void testeBuscarJogadorMaisVelho() {
+		DesafioMeuTimeApplication teste = new DesafioMeuTimeApplication();
+		
+		try {
+			teste.buscarJogadorMaisVelho(1L);
+			Assert.assertTrue("Deveria dar Exceção", false);
+		} catch (Exception e) {
+			Assert.assertTrue("Deveria ser TimeNaoEncontradoException", e instanceof TimeNaoEncontradoException);
+		}
+		
+		incluirTimes(teste);
+		incluirJogadores(teste);
+		
+		Assert.assertEquals("Deveria ser Leonardo1" , Long.valueOf(6), teste.buscarJogadorMaisVelho(2L));
+	}
+	
+	@Test
+	public void testeBuscarTimes() {
+		DesafioMeuTimeApplication teste = new DesafioMeuTimeApplication();
+		
+		Assert.assertEquals("Deveria ser Lista Vazia", new ArrayList<Long>(), teste.buscarTimes());
+		
+		incluirTimes(teste);
+		
+		ArrayList<Long> listatimes = new ArrayList<Long>();
+		listatimes.add(1L);
+		listatimes.add(2L);
+		listatimes.add(3L);
+		
+		Assert.assertEquals("Deveriam ser iguais", listatimes, teste.buscarTimes());
+	}
+	
+	@Test
+	public void testeBuscarJogadorMaiorSalario() {
+		DesafioMeuTimeApplication teste = new DesafioMeuTimeApplication();
+		
+		try {
+			teste.buscarJogadorMaiorSalario(2L);
+			Assert.assertTrue("Deveria dar Exceção", false);
+		} catch (Exception e) {
+			Assert.assertTrue("Deveria ser TimeNaoEncontradoException", e instanceof TimeNaoEncontradoException);
+		}
+		
+		incluirTimes(teste);
+		incluirJogadores(teste);
+		
+		Assert.assertEquals("Deveria ser Leonardo3", Long.valueOf(6), teste.buscarJogadorMaiorSalario(2L));
+	}
+	
+	@Test
+	public void testeBuscarSalarioDoJogador() {
+		DesafioMeuTimeApplication teste = new DesafioMeuTimeApplication();
+		
+		try {
+			teste.buscarSalarioDoJogador(6L);
+			Assert.assertTrue("Deveria dar Exceção", false);
+		} catch (Exception e) {
+			Assert.assertTrue("Deveria ser 0", e instanceof JogadorNaoEncontradoException);
+		}
+		
+		incluirTimes(teste);
+		incluirJogadores(teste);
+		
+		Assert.assertEquals("Deveria ser 0", BigDecimal.valueOf(0), teste.buscarSalarioDoJogador(5L));
+	}
+	
+	@Test
+	public void testeBuscarTopJogadores() {
+		DesafioMeuTimeApplication teste = new DesafioMeuTimeApplication();
+		
+		Assert.assertEquals("Deveria ser lista vazia", new ArrayList<Long>(), teste.buscarTopJogadores(3));
+		
+		incluirTimes(teste);
+		incluirJogadores(teste);
+		
+		ArrayList<Long> listadostop = new ArrayList<Long>();
+		listadostop.add(4L);
+		listadostop.add(5L);
+		listadostop.add(6L);
+		
+		Assert.assertEquals("Lista Incorreta", listadostop, teste.buscarTopJogadores(3));
+	}
+	
+	@Test
+	public void testeBucarCorCamisetaTimeDeFora() {
+		DesafioMeuTimeApplication teste = new DesafioMeuTimeApplication();
+		
+		try {
+			teste.buscarCorCamisaTimeDeFora(1L, 2L);
+			Assert.assertTrue("Deveria dar Exceção", false);
+		} catch (Exception e) {
+			Assert.assertTrue("Deveria ser TimeNaoEncontradoException", e instanceof TimeNaoEncontradoException);
+		}
+		
+		incluirTimes(teste);
+		
+		Assert.assertEquals("Deveria ser prata", "Prata", teste.buscarCorCamisaTimeDeFora(1L, 2L));
+		Assert.assertEquals("Deveria ser Vermelho", "Vermelho", teste.buscarCorCamisaTimeDeFora(1L, 3L));
+	}
 }
